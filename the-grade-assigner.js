9cdc1614adb954b87de03de6ade7a0a9bbd4ddf5
Eli Broadfoot <elibroadfoot@gmail.com>
2016-12-15 22:05:00 -0800

    All loop exercises

--- the-grade-assigner.js
+++ the-grade-assigner.js
@@ -10,3 +10,27 @@ logging each grade point in the range.
 */
 
 // YOUR CODE GOES HERE
+
+function assignGrade(grade) {
+    if (grade > 90) {
+        return 'A';
+    } else if (grade > 80) {
+        return 'B';
+    } else if (grade > 70) {
+        return 'C';
+    } else if (grade > 65) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+for (var i = 60; i <= 65; i++) {
+  console.log('For ' + i + ', you got an ' + assignGrade(i));
+}
+for (var i = 66; i <= 90; i++) {
+  console.log('For ' + i + ', you got a ' + assignGrade(i));
+}
+for (var i = 91; i <= 100; i++) {
+  console.log('For ' + i + ', you got an ' + assignGrade(i));
+}
